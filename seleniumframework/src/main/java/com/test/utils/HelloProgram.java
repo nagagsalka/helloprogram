b07784071f9ef813b47b7afaf768f5dd358f304f
hasan <hasan@yahoo.com>
2016-07-31 11:04:24 -0400

    hasan uploading project

---
+++ seleniumframework/src/main/java/com/test/utils/HelloProgram.java
@@ -0,0 +1,19 @@
+package com.test.utils;
+
+import org.testng.annotations.Test;
+
+public class HelloProgram {
+	
+	@Test
+	public void hello(){
+	
+		System.out.println("Hello world");
+	}
+	
+	@Test
+	public void helloShafiBhai(){
+	
+		System.out.println("helloShafiBhai");
+	}
+
+}
